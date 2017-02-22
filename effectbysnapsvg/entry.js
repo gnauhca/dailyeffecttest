6b2d085332d07c7c356c183ebfd1ab3ad91d11b4
zhzhchwin <576073927@qq.com>
2017-02-23 00:20:30 +0800

    group.animate({

--- effectbysnapsvg/entry.js
+++ effectbysnapsvg/entry.js
@@ -52,37 +52,153 @@
 	
 	console.log(Snap);
 	
-	var s = Snap(500, 500);
+	var s = Snap(1000, 1000);
 	
-	var circle = s.circle(0, 0, 100);
-	circle.attr({
-	    stroke: "#000",
-	    strokeWidth: 2,
-	    fill: 'none',
-	    'stroke-dasharray': "1,5",
-	    transform: 'translate(250, 250)'
+	var sElems = {};
 	
+	sElems.circle0 = s.circle(0, 0, 200);
+	sElems.circle0.attr({
+	    'stroke': "#495b89",
+	    'strokeWidth': 1,
+	    'fill': 'none'
 	});
-	Snap.animate(5, 4, function (val) {
-	    circle.attr({
-	        'stroke-dashoffset': val * 1000,
-	        'stroke-dasharray': "1," + val
 	
+	sElems.arc0 = s.circle(0, 0, 204);
+	sElems.arc0.attr({
+	    'stroke': "#8a9dbe",
+	    'strokeWidth': 8,
+	    'fill': 'none',
+	    'stroke-dasharray': '220 430'
+	    // 'stroke-dasharray': '0 ' + Math.PI * 2 * 204
+	});
+	
+	sElems.dashCircle0 = s.circle(0, 0, 193);
+	sElems.dashCircle0.attr({
+	    'stroke': "#8a9dbe",
+	    'strokeWidth': 1,
+	    'fill': 'none',
+	    'stroke-dasharray': '1 5'
+	});
+	
+	Snap.animate([100, 400], [1, 5], function (v) {
+	    sElems.dashCircle0.attr({
+	        'stroke-dasharray': v.join(' '),
+	        'stroke-dashoffset': v[0] + v[1]
 	    });
-	}, 6000);
+	}, 1000, '', mina.bounce);
 	
-	circle.animate({
-	    r: 80,
-	    'stroke-dasharray': "1,1",
-	    transform: 'translate(250, 250) skewX(30)'
-	}, 1000);
-	circle.transform();
+	////////////
+	
+	
+	sElems.circle2 = s.circle(0, 0, 250);
+	sElems.circle2.attr({
+	    'stroke': "#495b89",
+	    'strokeWidth': 1,
+	    'fill': 'none'
+	});
+	
+	sElems.dashCircle2 = s.circle(0, 0, 241);
+	sElems.dashCircle2.attr({
+	    'stroke': "#8a9dbe",
+	    'strokeWidth': 8,
+	    'fill': 'none',
+	    'stroke-dasharray': '1 10'
+	});
+	
+	////////////
+	
+	
+	sElems.circle3 = s.circle(0, 0, 100);
+	sElems.circle3.attr({
+	    'stroke': "#bb216a",
+	    'strokeWidth': 1,
+	    'fill': 'none'
+	});
+	
+	sElems.arc3 = s.circle(0, 0, 102);
+	sElems.arc3.attr({
+	    'stroke': "#bb216a",
+	    'strokeWidth': 5,
+	    'fill': 'none',
+	    'stroke-dasharray': '150 160'
+	});
+	
+	////////////
+	sElems.dashCircle4 = s.circle(0, 0, 81);
+	sElems.dashCircle4.attr({
+	    'stroke': "#8a9dbe",
+	    'strokeWidth': 1,
+	    'fill': 'none',
+	    'stroke-dasharray': '1 5'
+	});
+	
+	////////////
+	sElems.circle5 = s.circle(0, 0, 60);
+	sElems.circle5.attr({
+	    'stroke': "#9293b1",
+	    'strokeWidth': 2,
+	    'fill': 'none'
+	});
+	
+	////////////
+	sElems.dashCircle6 = s.circle(0, 0, 51);
+	sElems.dashCircle6.attr({
+	    'stroke': "#8a9dbe",
+	    'strokeWidth': 1,
+	    'fill': 'none',
+	    'stroke-dasharray': '1 6'
+	});
+	
+	///////////
+	sElems.dashCircle7 = s.circle(0, 0, 41);
+	sElems.dashCircle7.attr({
+	    'stroke': "#8a9dbe",
+	    'strokeWidth': 1,
+	    'fill': 'none',
+	    'stroke-dasharray': '1 8'
+	});
+	
+	///////////
+	sElems.circle8 = s.circle(0, 0, 18);
+	sElems.circle8.attr({
+	    'stroke': "none",
+	    'fill': '#788ebd'
+	});
+	
+	var group = s.group();
+	
+	for (var key in sElems) {
+	    group.append(sElems[key]);
+	    var x = key.match(/\d$/)[0];
+	    sElems[key].attr({
+	        'transform': 'translate(' + x * 50 + ' ' + x * 30 + ')'
+	    });
+	}
+	
+	Snap.animate(0, 100000, function (r) {
+	    for (var key in sElems) {
+	        var x = key.match(/\d$/)[0];
+	        var sign = x % 2 ? 1 : -1;
+	
+	        sElems[key].attr({
+	            'transform': 'translate(' + x * 50 + ' ' + x * 30 + ') rotate(' + sign * r + ')'
+	        });
+	    }
+	}, 10000000);
+	
+	group.attr({
+	    'transform': 'translate(500 600) scale(2)'
+	});
+	
+	group.animate({
+	    'transform': 'translate(500 500) skewX(-30)'
+	}, 4000);
 
 /***/ },
 /* 1 */
 /***/ function(module, exports, __webpack_require__) {
 
-	var __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;var __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_LOCAL_MODULE_0__;/*** IMPORTS FROM imports-loader ***/
+	var __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_LOCAL_MODULE_0__;var __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;/*** IMPORTS FROM imports-loader ***/
 	(function() {
 	var fix = module.exports=0;
 	
