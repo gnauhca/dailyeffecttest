c350fdecf890f118278d028a1ba70230abe195db
gnauhca <chazzhou@tencent.com>
2017-09-12 21:54:27 +0800

    update

--- ios-select/index.js
+++ ios-select/index.js
@@ -18,7 +18,11 @@ class IosSelector {
 
     this.halfCount = this.options.count / 2;
     this.quarterCount = this.options.count / 4;
-    this.a = this.options.sensitivity * 2; // 滚动减速度
+    this.a = this.options.sensitivity * 10; // 滚动减速度
+    this.minV = Math.sqrt(1 / this.a); // 最小初速度
+
+    this.exceedA = 100; // 超出减速 
+    this.moveT = 0; // 滚动 tick
 
     this.elems = {
       el: document.querySelector(this.options.el),
@@ -63,13 +67,15 @@ class IosSelector {
     document.addEventListener('touchstart', this.events.touchstart);
     // document.addEventListener('touchmove', this.events.touchmove);
     document.addEventListener('touchend', this.events.touchend);
-    this.moveTo(0);
+    this._moveTo(0);
   }
 
   _touchstart(e, touchData) {
     document.addEventListener('touchmove', this.events.touchmove);
     touchData.startY = e.touches[0].clientY;
     touchData.startTime = new Date().getTime();
+    window.cancelAnimationFrame(this.moveT);
+
     console.log('start');
   }
 
@@ -78,17 +84,30 @@ class IosSelector {
     touchData.endY = e.touches[0].clientY;
 
     let scrollAdd = (touchData.startY - touchData.endY) / this.itemHeight;
-    touchData.touchScroll = this.moveTo(this.scroll + scrollAdd);
+    let moveToScroll = scrollAdd + this.scroll;
+
+    // 非无限滚动时，超出范围使滚动变得困难
+    if (this.type === 'normal') {
+      if (moveToScroll < 0) {
+        moveToScroll *= 0.3;
+      } else if (moveToScroll > this.source.length) {
+        moveToScroll = this.source.length + (moveToScroll - this.source.length) * 0.3;
+      }
+    }
+
+    touchData.touchScroll = this._moveTo(this.scroll + scrollAdd);
   }
 
   _touchend(e, touchData) {
     document.removeEventListener('touchmove', this.events.touchmove);
     // 计算速度
     let endTime = new Date().getTime();
-    let v = (touchData.touchScroll - this.scroll) * 1000 / (new Date().getTime() - touchData.startTime);
+    let v = ((touchData.startY - touchData.endY) / this.itemHeight) * 1000 / (new Date().getTime() - touchData.startTime);
+    let sign = v > 0 ? 1 : -1;
 
+    v = Math.abs(v) > 30 ? 30 * sign : v;
     this.scroll = touchData.touchScroll;
-    this.move(v);
+    this._move(v);
 
     console.log('end');
   }
@@ -137,8 +156,18 @@ class IosSelector {
                     >${source[i].text}</li>`
     }
 
+    // 中间高亮 HTML
+    let highListHTML = '';
+    for (let i = 0; i < source.length; i++) {
+      highListHTML += `<li class="highlight-item" style="height: ${this.itemHeight}px;">
+                        ${source[i].text}
+                      </li>`
+    }
+
+
     if (this.options.type === 'infinite') {
 
+      // 圆环头尾
       for (let i = 0; i < this.quarterCount; i++) {
         // 头
         circleListHTML = `<li class="select-option"
@@ -157,22 +186,13 @@ class IosSelector {
                       data-index="${i + sourceLength}"
                       >${source[i].text}</li>`;
       }
+
+      // 高亮头尾
+      highListHTML = `<li class="highlight-item" style="height: ${this.itemHeight}px;">
+                          ${source[sourceLength - 1].text}
+                      </li>` + highListHTML;
+      highListHTML += `<li class="highlight-item" style="height: ${this.itemHeight}px;">${source[0].text}</li>`
     }
-
-
-    // 中间高亮 HTML
-    let highListHTML = '';
-    for (let i = 0; i < source.length; i++) {
-      highListHTML += `<li class="highlight-item" style="height: ${this.itemHeight}px;">
-                        ${source[i].text}
-                      </li>`
-    }
-    // 高亮头尾补一个
-    highListHTML = `<li class="highlight-item" style="height: ${this.itemHeight}px;">
-                        ${source[sourceLength - 1].text}
-                    </li>` + highListHTML;
-    highListHTML += `<li class="highlight-item" style="height: ${this.itemHeight}px;">${source[0].text}</li>`
-
 
     this.elems.el.innerHTML = template
                                 .replace('{{circleListHTML}}', circleListHTML)
@@ -184,15 +204,14 @@ class IosSelector {
     this.elems.highlightitem = this.elems.el.querySelectorAll('.highlight-item');
   }
 
-  move(initV) {
 
-  }
-
-  moveTo(scroll) {
-    while(scroll < 0) {
-      scroll += this.source.length;
+  _moveTo(scroll) {
+    if (this.type === 'infinite') {
+      while(scroll < 0) {
+        scroll += this.source.length;
+      }
+      scroll = scroll % this.source.length;
     }
-    scroll = scroll % this.source.length;
 
     this.elems.circleList.style.transform = `translateZ(${-this.radius}px) rotateX(${this.itemAngle * scroll}deg)`;
          
@@ -204,9 +223,62 @@ class IosSelector {
       }
     });
 
-    console.log(scroll);
+    // console.log(scroll);
     // console.log(`translateZ(${-this.radius}px) rotateX(${-this.itemAngle * scroll}deg)`);
     return scroll;
+  }
+
+
+  _move(initV) {
+
+    console.log(initV);
+
+    let start = new Date().getTime() / 1000;
+    let pass = 0;
+    let initScroll = this.scroll;
+
+    let a = initV > 0 ? -this.a : this.a; // 减速加速度
+    let t = Math.abs(initV / a); // 速度减到 0 花费时间
+    let totalScrollLen = initV * t + a * t * t / 2; // 总滚动长度
+    let finalScroll = Math.round(this.scroll + totalScrollLen); // 取整，确保准确最终 scroll 为整数
+
+    totalScrollLen = finalScroll - this.scroll;
+
+    // 取整后反推加速度，初速度
+    a = totalScrollLen > 0 ? -this.a : this.a; 
+    t = Math.sqrt(totalScrollLen * 2 / -a);
+    initV = -a * t;
+
+    console.log(initV, t, a, totalScrollLen);
+
+    // scroll 取整后，微调 initV 
+    initV = (totalScrollLen - a * t * t / 2) / t;
+
+    let moveScrollLen;
+
+    let tick = () => {
+      pass = new Date().getTime() / 1000 - start;
+
+      if (pass < t) {
+        moveScrollLen = initV * pass + a * pass * pass / 2;
+        this.moveT = window.requestAnimationFrame(tick);
+      } else {
+        moveScrollLen = totalScrollLen;
+        this.scroll = finalScroll;
+      }
+      this._moveTo(initScroll + moveScrollLen);
+    }
+    tick();
+  }
+
+  // 超出部分，非 infinite 情况
+  _exceedMove(initV) {
+
+  }
+
+  // 回弹
+  _exceedBackMove() {
+
   }
 
   updateSource(source) {
