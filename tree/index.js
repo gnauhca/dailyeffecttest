7917a36457911ee633326ff4b859cfa5afc1aaa6
gnauhca <576073927@qq.com>
2017-11-17 21:07:32 +0800

    update

--- tree/index.js
+++ tree/index.js
@@ -1,6 +1,6 @@
 import './index.scss';
 import Stats from 'stats.js';
-// import * as THREE from 'three';
+import * as THREE from 'three';
 import { defaultsDeep } from 'lodash';
 import { Time, TIME, TWEEN } from './time.js';
 import TrackballControls from './trackball.js';
@@ -92,7 +92,7 @@ class Branch {
     this.id = Branch.id++;
     this.tree = tree;
     this.parent = parent;
-    this.deep = this.parent ? this.parent.deep + 1 : 0;
+    this.deep = this.parent ? this.parent.deep + (this.endAtPercent < 1 ? 0 : 1) : 0;
     this.start = this.parent ? parent.end : new THREE.Vector3; // 开始点
 
     let tmp = this.length * Math.cos(this.angles[1]);
@@ -111,7 +111,7 @@ class Branch {
     let progressSegment = this.endAtPercent - this.startAtPercent;
 
     // 生长速度 百分比 / 每秒
-    this.speed = this.tree.maxSpeed * (this.tree.maxDeep - this.deep) / this.tree.maxDeep / progressSegment;
+    this.speed = this.tree.maxSpeed * (this.tree.maxDeep - this.deep) / this.tree.maxDeep;
 
     // this.update();
     this.tree.addBranch(this);
@@ -120,20 +120,20 @@ class Branch {
   // grow
   grow(delta) {
 
-    if (this.progress === 1) {
+    if (this.progress === this.endAtPercent) {
       // TODO: 树枝摆动, wind
       return;
     }
     
     this.progress += delta / this.speed / 1000;
-    this.progress = this.progress > 1 ? 1 : this.progress;
+    this.progress = Math.min(this.progress, this.endAtPercent);;
     this.end.addVectors(this.start, this.vector.clone().multiplyScalar(this.progress));
 
-    if (this.progress < 1) { 
+    if (this.progress < this.endAtPercent) { 
       return; 
     }
 
-    if (this.endAtPercent < 1) {
+    if (this.endAtPercent < 0.7) {
       // 长出一只同级树枝，此时不是分裂，而是为了实现曲线生长
       let angles = [
         this.angles[0] + (Math.random() - 0.5) * RADIAN * 20,
@@ -173,7 +173,7 @@ class Branch {
       let cRadiusStart = this.radiusEnd;
       let cRadiusEnd = Math.max(cRadiusStart - this.tree.radiusReduceSpeed * cOptions.length, 0.2);
       let cAngles = [
-        this.angles[0] + 10 * RADIAN * sign1 * -1,
+        this.angles[0] + 20 * RADIAN * sign1 * -1,
         this.angles[1] + 10 * RADIAN * sign2 * -1
       ];
       let cBranch = new Branch(this.tree, this, Object.assign({
@@ -185,10 +185,10 @@ class Branch {
 
       // 非直系 isolated
       let iOptions = this.generateChildBaseOptions();
-      let iRadiusStart = this.radiusEnd * (0.6 + Math.random() * 0.3); 
+      let iRadiusStart = this.radiusEnd * (0.7 + Math.random() * 0.3); 
       let iRadiusEnd = Math.max(iRadiusStart - this.tree.radiusReduceSpeed * iOptions.length, 0.2);
       let iAngles = [
-        this.angles[0] + 30 * RADIAN * sign3,
+        this.angles[0] + 40 * RADIAN * sign3,
         this.angles[1] + -30 * RADIAN * sign4 * -1
       ];
       let iBranch = new Branch(this.tree, this, Object.assign({
@@ -197,23 +197,29 @@ class Branch {
         radiusEnd: iRadiusEnd,
         isIsolate: true,
       }, iOptions));
+      iBranch.deep = Math.min(this.tree.maxDeep, Math.round(Math.random()) + iBranch.deep);
+
       this.isolatedChildren.push(iBranch);
     }
 
   }
 
   generateChildBaseOptions() {
-    let length = this.tree.maxDeep * this.tree.maxLength * (0.5 + 0.5 * Math.random()) / (this.deep + 1 + this.tree.maxDeep)
+    let length = this.tree.maxDeep * this.tree.maxLength * (0.5 + 0.5 * Math.random()) / (this.deep ** 1.5 + 1 + this.tree.maxDeep)
+    let startAtPercent = 0;
+    let endAtPercent = Math.random() > 0.5 ? Math.random() * 0.3 + 0.3 : 1;
 
     return {
-      startAtPercent: 0,
-      // endAtPercent: Math.random() > 0.5 ? Math.random() * 0.3 + 0.3 : 1,
+      startAtPercent,
+      endAtPercent,
+      percentSegment: endAtPercent - startAtPercent,
       length: length,
       branchLength: length,
       speed: 0.5 + Math.random() * 0.5
     };
   }
 }
+
 Branch.id = 0;
 
 class Tree {
@@ -254,7 +260,7 @@ class Tree {
       isIsolate: true,
       angles: [0, 90 * RADIAN],
       radiusStart: this.rootRadius,
-      radiusEnd: this.rootRadius,
+      radiusEnd: this.rootRadius * 0.8,
       length: this.maxLength,
       branchLength: this.maxLength,
       speed: this.maxSpeed
@@ -263,7 +269,7 @@ class Tree {
 
   getPointNumByBranchRadius(radius) {
     // return 10;
-    return Math.max(4, Math.min(parseInt(radius / 0.3), 8));
+    return Math.max(3, Math.min(parseInt(radius / 0.3), 10));
   }
 
   addBranch(branch) {
@@ -385,17 +391,30 @@ class Ani extends Time {
     this.scene = new THREE.Scene();//场景
 
     this.camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 3000);//透视相机
-    this.camera.position.set(0, 0, 200);//相机位置
+    this.camera.position.set(0, 50, 150);//相机位置
     this.scene.add(this.camera);//add到场景中
-    this.camera.lookAt(new THREE.Vector3(0, 100, 0));
+    this.camera.lookAt(new THREE.Vector3(0, 0, 0));
     // this.scene.fog = new THREE.Fog(0x000000, 100, 500);
     this.control = new TrackballControls(this.camera, document.body);
     // this.control.travel = true;
     this.travelSpeed = 20000;
 
+
+
+
     this.spotLight = new THREE.SpotLight(0xffffff);
     this.scene.add(this.spotLight);
     this.spotLight.position.set(-200, -200, 100);
+
+    this.spotLight2 = this.spotLight.clone();
+    this.scene.add(this.spotLight2);
+    this.spotLight2.position.set(50, 500, -100);
+
+    this.planeGeom = new THREE.PlaneGeometry(200, 200);
+    this.planeGeom.rotateX(1.577)
+    this.planeMaterial = new THREE.MeshBasicMaterial({color: 0xcccccc, wireFrames: 1, side: THREE.DoubleSide});
+    this.plane = new THREE.Mesh(this.planeGeom, this.planeMaterial);
+    this.scene.add(this.plane);
 
     this.renderer = new THREE.WebGLRenderer({antialias: true , alpha: true});//渲染
     this.renderer.setClearColor(0x000000, 0);
